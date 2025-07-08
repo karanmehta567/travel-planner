5716b298887514e38fb5fd6b1befba7970601911
karanmehta567 <karanmehta29.km@gmail.com>
2025-07-08 23:50:19 +0530

    Add recommended ESLint configuration for improved linting support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,9 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    extends: ['eslint:recommended'],
+  },
 });
 
 export default [
