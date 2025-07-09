332546d46020eb4e74e2cc9567b31af7d7b499dc
karanmehta567 <karanmehta29.km@gmail.com>
2025-07-09 16:47:51 +0530

    Added Analytics for monitoring views

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { auth } from "@/next-auth";
-
+import { Analytics } from "@vercel/analytics/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,6 +32,7 @@ export default async function RootLayout({
       >
         <Navbar session={session}/>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
